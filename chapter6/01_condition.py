4f88ddbdba86c0020da1d0b6f7825a5a3e784933
imdad123 <uimdad19@gmail.com>
2025-05-15 22:18:31 +0500

    -

--- chapter6/01_condition.py
+++ chapter6/01_condition.py
@@ -1,4 +1,8 @@
 user_input = int(input("Enter your age"))
 
-if user_input < 18:
+if user_input >= 18:
     print("You are not eligible to vote")
+elif user_input < 0:
+    print("Wrong input")
+else:
+    print("Above the mentioned Age")
