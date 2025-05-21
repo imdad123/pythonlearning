248c03c822ab0eade6c99838a38cdfd31bfed243
imdad <uimdad19@gmail.com>
2025-05-21 05:52:02 +0500

    chapter 8

--- chapter1/module.py
+++ chapter1/module.py
@@ -11,3 +11,26 @@ this is a multi-line comment
 #  use pip install to install packages external
 #simple line comment
 # repl(Read, Evaluate, Print, and Loop) its the terminal when we type python in terminal and code there r
+
+
+# Built in modules Mostely Used
+# os
+# Purpose: Interact with the operating system
+# sys
+# Purpose: Access system-specific parameters and functions.
+# math
+# Purpose: Mathematical functions
+# datetime
+# Purpose: Date and time manipulation
+# re
+# Purpose: Regular expression matching.
+# json
+# Purpose: JSON parsing and generation.
+# random
+# Purpose: Generate pseudo-random numbers.
+# collections
+# Purpose: Specialized container datatypes.
+# itertools
+# Purpose: Efficient looping tools.
+# unittest
+# Purpose: Unit testing framework.
---
+++ chapter8/02_quiz.py
@@ -0,0 +1,5 @@
+def greet_user(name):
+    print(f"Hello, {name}!")
+
+user_input = input("Enter your name: ")
+greet_user(user_input)
